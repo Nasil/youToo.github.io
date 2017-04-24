2368c88724789b5c868a96839472c58e5ae66f4c
Nasil <Nasil@users.noreply.github.com>
2017-04-24 15:20:58 +0900

    Create getImages2.js

---
+++ node/getImages2.js
@@ -0,0 +1,49 @@
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var path = require("path");
+var url = require("url");
+var events = require("events").EventEmitter;
+var util = require("util");
+var cheerio = require("cheerio");
+
+const TARGET_URL = 'http://www.secretlabel.co.kr/';
+parseImg(TARGET_URL, 0)
+
+
+function parseImg(utl, level) {
+
+    // Support HTTPS.
+    var protocol = http;
+    var request = protocol.request(utl, function(response){
+        if(response.statusCode != 200){
+            onsole.log('HAHA');
+            console.error("Image scraper(1): web page couldn't be found. (statusCode:" + response.statusCode + ")");
+            request.end();
+            return process.exit(1);
+        }
+        else{
+            response.setEncoding("utf8");
+            var previous = "";
+            var current;
+            response.on("data", function(data){
+                var current = previous + data;
+                current.replace(/<img[\S\s]*?>/ig, function(m){
+                    let imageInfo = cheerio.load(m)("img")[0];
+                    let at = imageInfo.attribs;
+                    let name = path.basename(at.src, path.extname(at.src)); // img name
+                    let pullUrl = at.src;
+                    let saveTo = path.dirname(require.main.filename)
+                    let extension = path.extname(at.src); // img type
+                    let savepath = saveTo + '\\' + name + extension;
+                    console.log(pullUrl);
+                });
+                previous = data;
+            });
+        }
+    });
+    request.end();
+    request.on("error", function(e){
+        console.error("Image scraper(2): error while loading web page: " + e + ".");
+    });
+};
