92323265b563b3d1d26e2c1757445c9c51b37c53
Nasil <Nasil@users.noreply.github.com>
2017-09-02 22:06:18 +0900

    Update basic.js

--- JS/basic.js
+++ JS/basic.js
@@ -44,5 +44,27 @@ console.log(Math.pow(2,3));
 console.log(Math.sqrt(16));
 console.log(Math.random());
 
+// array
+var arr = [1,2,3,4];
 
+arr.pop();
+console.log(arr);  // [ 1, 2, 3 ]
+arr.push("end");
+arr.unshift('begin');
+console.log(arr); // [ 'begin', 1, 2, 3, 'end' ]
+arr.reverse();
+console.log(arr); // [ 'end', 3, 2, 1, 'begin' ]
+arr.sort();
+console.log(arr); // [ 1, 2, 3, 'begin', 'end' ]
+console.log(arr.indexOf(2)); // 1
+console.log(arr.lastIndexOf(2)); // 1
+
+var arr1 = [1,2,3];
+var arr2 = [4,5,6];
+var arr3 = arr1.concat(arr2);
+console.log(arr3); // [ 1, 2, 3, 4, 5, 6 ]
+
+var str = "1,2,3,4,5";
+var arrStr = str.split(","); 
+console.log(arrStr); // [ '1', '2', '3', '4', '5' ]
 
