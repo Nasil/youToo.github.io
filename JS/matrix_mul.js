ff54d494ce3b854bcb70519a511f17d8a33043c6
Nasil <Nasil@users.noreply.github.com>
2017-09-03 21:26:10 +0900

    Update matrix_mul.js

--- JS/matrix_mul.js
+++ JS/matrix_mul.js
@@ -1,7 +1,5 @@
 function productMatrix(A, B) {
 	var answer = Array();
-
-	var a = 0;
 	for (var a = 0; a < A[0].length; a++) {
 		var tmp = Array();
 		for (var b = 0; b < B.length; b++ ){ 
@@ -16,11 +14,16 @@ function productMatrix(A, B) {
 			answer[a] = tmp;
 		}
 	}
-	
+
 	return answer;
 }
+
+const productMatrix2 = (A, B) => A.map(a => B.map((b, j) =>
+    a.reduce((ap, ac, k) => ap + ac * B[k][j], 0
+)));
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
 var a = [ [1,2,3],[4,5,6] ];
 var b = [ [1,2],[3,4],[5,6] ];
 console.log("결과 : " + productMatrix(a, b));
+console.log("결과 : " + productMatrix2(a, b));
