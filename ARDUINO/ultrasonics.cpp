649e561df27efff0e39471f27548e1f87f2de9fa
Nasil <Nasil@users.noreply.github.com>
2017-10-22 21:19:59 +0900

    Update ultrasonics.cpp

--- ARDUINO/ultrasonics.cpp
+++ ARDUINO/ultrasonics.cpp
@@ -1,7 +1,15 @@
-const int trigPin1 = 2; 
-const int echoPin1 = 3;
-const int trigPin2 = 4; 
-const int echoPin2 = 5;   
+const int trigPin1 = 3; 
+const int echoPin1 = 2;
+const int trigPin2 = 5; 
+const int echoPin2 = 4; 
+const int trigPin3 = 7; 
+const int echoPin3 = 6; 
+const int trigPin4 = 9; 
+const int echoPin4 = 8; 
+const int trigPin5 = 11; 
+const int echoPin5 = 10; 
+const int trigPin6 = 13; 
+const int echoPin6 = 12;   
 
 void setup()
 {
@@ -11,17 +19,37 @@ void setup()
     pinMode(trigPin1, OUTPUT);  
     pinMode(echoPin1, INPUT);
     pinMode(trigPin2, OUTPUT);  
-    pinMode(echoPin2, INPUT);   
+    pinMode(echoPin2, INPUT); 
+    pinMode(trigPin3, OUTPUT);  
+    pinMode(echoPin3, INPUT); 
+    pinMode(trigPin4, OUTPUT);  
+    pinMode(echoPin4, INPUT);
+    pinMode(trigPin5, OUTPUT);  
+    pinMode(echoPin5, INPUT);
+    pinMode(trigPin6, OUTPUT);  
+    pinMode(echoPin6, INPUT);    
 }
 
 void loop()
 {
     float ultraTime1 = readUltrasonic(trigPin1, echoPin1);
     float ultraTime2 = readUltrasonic(trigPin2, echoPin2);
+    float ultraTime3 = readUltrasonic(trigPin3, echoPin3);
+    float ultraTime4 = readUltrasonic(trigPin4, echoPin4);
+    float ultraTime5 = readUltrasonic(trigPin5, echoPin5);
+    float ultraTime6 = readUltrasonic(trigPin6, echoPin6);
 
     Serial.print(ultraTime1);
     Serial.print("cm - ");
     Serial.print(ultraTime2);
+    Serial.print("cm - ");
+    Serial.print(ultraTime3);
+    Serial.print("cm - ");
+    Serial.print(ultraTime4);
+    Serial.print("cm - ");
+    Serial.print(ultraTime5);
+    Serial.print("cm - ");
+    Serial.print(ultraTime6);
     Serial.print("cm");
     Serial.println();
 
