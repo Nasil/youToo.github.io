1be486d054120e68a7d199dd4dd5ede3dea1e3da
Nasil <Nasil@users.noreply.github.com>
2018-02-06 14:23:46 +0900

    Create canny_node.js

---
+++ computerVision/canny_node.js
@@ -0,0 +1,80 @@
+```
+"use strict";
+
+var SOBEL_X_FILTER = [[-1, 0, 1],
+                        [-2, 0, 2],
+                        [-1, 0, 1]];
+var SOBEL_Y_FILTER = [[1, 2, 1],
+                        [0, 0, 0],
+                        [-1, -2, -1]];
+var ROBERTS_X_FILTER = [[1, 0],
+                        [0, -1]];
+var ROBERTS_Y_FILTER = [[0, 1],
+                        [-1, 0]];
+var PREWITT_X_FILTER = [[-1, 0, 1],
+                        [-1, 0, 1],
+                        [-1, 0, 1]];
+var PREWITT_Y_FILTER = [[-1, -1, -1],
+                        [0, 0, 0],
+                        [1, 1, 1]];
+
+var OPERATORS = { "sobel": {
+                    x: SOBEL_X_FILTER,
+                    y: SOBEL_Y_FILTER,
+                    len: SOBEL_X_FILTER.length
+                  },
+                  "roberts": {
+                    x: ROBERTS_X_FILTER,
+                    y: ROBERTS_Y_FILTER,
+                    len: ROBERTS_Y_FILTER.length
+                  },
+                  "prewitt": {
+                    x: PREWITT_X_FILTER,
+                    y: PREWITT_Y_FILTER,
+                    len: PREWITT_Y_FILTER.length
+                  }
+                };
+
+function gradient(frame, column, row) {
+    console.time('Sobel Filter Time');
+
+    //let row = height;
+    //let column = width;
+
+    var image_x = new Uint8ClampedArray(column * row);
+    var image_y = new Uint8ClampedArray(column * row);
+
+    for (var i = 0; i < row; i++) {
+      for (var j = 0; j < column; j++) {
+        let pixel = 0;
+        let x_convolution = 0;
+        let y_convolution = 0;
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <=1; y++) {
+                pixel = (i == 0 || i == row-1 || j == 0 || j == column-1) ? 0 : frame[(i+x) * column + (j+y)];
+                x_convolution = x_convolution + (pixel * SOBEL_X_FILTER[1+x][1+y]);
+                y_convolution = y_convolution + (pixel * SOBEL_Y_FILTER[1+x][1+y]);
+            }
+        }
+        image_x[i * column + j] = x_convolution;
+        image_y[i * column + j] = y_convolution;
+      }
+    }
+
+    for (let i = 0; i < row; ++i) {
+        for (let j = 0; j < column; ++j) {
+            let imagex = image_x[i * column + j];
+            let imagey = image_y[i * column + j];
+            let gradient_approximation = Math.sqrt(imagex * imagex + imagey * imagey);
+            frame[i * column + j] = gradient_approximation;
+        }
+    }
+    console.timeEnd('Sobel Filter Time');
+
+    return frame;
+};
+
+
+exports.gradient = gradient;
+```
+                                
