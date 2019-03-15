8b214410062d8ffa457a1ea00e740f45f15655a2
Nasil <Nasil@users.noreply.github.com>
2019-03-15 13:47:06 +0900

    Update funcProgramming.js

--- JS/funcProgramming.js
+++ JS/funcProgramming.js
@@ -102,5 +102,26 @@ var expected = 5;
 if (result !== expected) {
 	throw new Error(result + ' is not equal to ' + expected);
 }
-```
 
+// 에러 함수
+function test(title, testCode) {
+  try {
+    testCode();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function expect(result) {
+  return {
+    toBe: function(expected) {
+      if (result !== expected) {
+      	throw new Error(result + ' is not equal to ' + expected);
+      }
+    }
+  }
+}
+
+test('sum(1, 2) is not equal 5', function() {
+  expect(sum(1, 2)).toBe(5);
+});
